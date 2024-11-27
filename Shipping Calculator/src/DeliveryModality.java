a80cc3f6e176addb7df3763149e2bceddf43e5e2
LB_TREE\kekoa <906530+kaurinz@users.noreply.github.com>
2024-11-27 15:41:18 -0500

    add additional properties to DeliveryModality

--- Shipping Calculator/src/DeliveryModality.java
+++ Shipping Calculator/src/DeliveryModality.java
@@ -1,6 +1,13 @@
 public class DeliveryModality {
     private String name;
+    private double maxAllowableDimension;
+    private double maxWeight;
 
-    public void setName(String name){this.name = name;}
-    public String getName(){return this.name;}
+    public String getName(){ return this.name; }
+    public double getMaxAllowableDimension() { return maxAllowableDimension; }
+    public double getMaxWeight() { return maxWeight; }
+
+    public void setName(String name){ this.name = name; }
+    public void setMaxAllowableDimension(double maxAllowableDimension) { this.maxAllowableDimension = maxAllowableDimension; }
+    public void setMaxWeight(double maxWeight) { this.maxWeight = maxWeight; }
 }
