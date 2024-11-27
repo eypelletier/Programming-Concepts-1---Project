f127cfb2462056b169e8b7b0ed974066dd9c7872
LB_TREE\kekoa <906530+kaurinz@users.noreply.github.com>
2024-11-27 16:12:00 -0500

    add code related to restrictions to DeliveryModality class

--- Shipping Calculator/src/DeliveryModality.java
+++ Shipping Calculator/src/DeliveryModality.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class DeliveryModality {
     private String name;
     private double maxAllowableDimension;
     private double maxWeight;
+    private ArrayList<GoodsCategory> goodsRestrictions;
 
     public String getName(){ return this.name; }
     public double getMaxAllowableDimension() { return maxAllowableDimension; }
@@ -10,4 +13,13 @@ public class DeliveryModality {
     public void setName(String name){ this.name = name; }
     public void setMaxAllowableDimension(double dimension) { this.maxAllowableDimension = dimension; }
     public void setMaxWeight(double weight) { this.maxWeight = weight; }
+
+    public DeliveryModality addGoodsRestriction(ArrayList<GoodsCategory> goodsRestrictionsList){
+        this.goodsRestrictions.addAll(goodsRestrictionsList);
+        return this;
+    }
+
+    public boolean canTransportGoodsCategory(GoodsCategory goodsCategory){
+        return !(goodsRestrictions.contains(goodsCategory));
+    }
 }
