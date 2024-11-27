8d35798a876c11a733dc5f489d368f0ead63d815
LB_TREE\kekoa <906530+kaurinz@users.noreply.github.com>
2024-11-27 15:59:29 -0500

    remove modality restrictions from GoodsCategory

--- Shipping Calculator/src/GoodsCategory.java
+++ Shipping Calculator/src/GoodsCategory.java
@@ -3,17 +3,10 @@ import java.util.ArrayList;
 public class GoodsCategory {
     private String name;
     private double surchargeRate;
-    private ArrayList<DeliveryModality> modalityRestrictions; 
 
     public GoodsCategory setSurchargeRate(double rate){this.surchargeRate = rate; return this;}
     public GoodsCategory setName(String name){this.name = name; return this; }
 
-    public GoodsCategory addRestriction(DeliveryModality[] modalityList){
-        //TODO
-        return this;
-    }
-
     public double getSurchargeRate(){return this.surchargeRate;}
     public String getName() {return this.name;}
-    public ArrayList<DeliveryModality> getModalityRestrictions(){ return this.modalityRestrictions;}
 }
