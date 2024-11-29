5e15ba04bf1086b7658b475352d6c3954dfb68ac
LB_TREE\kekoa <906530+kaurinz@users.noreply.github.com>
2024-11-29 15:43:57 -0500

    modify DeliveryStandard to streamline object creation

--- Shipping Calculator/src/DeliveryStandard.java
+++ Shipping Calculator/src/DeliveryStandard.java
@@ -1,17 +1,51 @@
-public enum DeliveryStandard {
-    STANDARD(100, 200, 150, 1000),
-    EXPRESS(50, 150, 100, 500);
-
+public class DeliveryStandard {
+    private String type;
     private final double maxHeight;
     private final double maxLength;
     private final double maxWidth;
     private final double maxWeight;
+
+    public static DeliveryStandard CreateStandard(String type){
+            DeliveryStandard newStandard = null;
+            switch (type.toLowerCase()){
+                case "standard":
+                    newStandard = DeliveryStandard.Standard();
+                    break;
+                case "express":
+                    newStandard = DeliveryStandard.Express();
+                    break;
+            }
+
+            if (newStandard != null){
+                newStandard.setType(type);
+            }
+
+            return newStandard;
+    }
+
+    private static DeliveryStandard Express() {
+         return new DeliveryStandard(50, 150, 100, 500);
+    }
+
+    private static DeliveryStandard Standard(){
+        return new DeliveryStandard(100, 200, 150, 1000);
+    }
+
+    // DeliveryStandard.ExpressStandard();
 
     DeliveryStandard(double maxHeight, double maxLength, double maxWidth, double maxWeight) {
         this.maxHeight = maxHeight;
         this.maxLength = maxLength;
         this.maxWidth = maxWidth;
         this.maxWeight = maxWeight;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType(){
+        return type;
     }
 
     public double getMaxHeight() {
