2087c89345f99dc54522270cc04c6726fd8883a0
LB_TREE\kekoa <906530+kaurinz@users.noreply.github.com>
2024-11-29 15:06:15 -0500

    basic ui interface sketch

--- Shipping Calculator/src/ShippingCalculator.java
+++ Shipping Calculator/src/ShippingCalculator.java
@@ -1,10 +1,12 @@
 import java.util.Scanner;
+import ui.HomeWindow;
 
 public class ShippingCalculator {
     public static void main(String[] args) {
+        HomeWindow hw = new HomeWindow();
 
+        /*
         Scanner input = new Scanner(System.in);
-
         System.out.print("Enter height in cm: ");
         double height = input.nextDouble();
         System.out.print("Enter width in cm: ");
@@ -13,6 +15,6 @@ public class ShippingCalculator {
         double length = input.nextDouble();
         System.out.print("Enter weight in kg: ");
         double weight = input.nextDouble();
-
+        */
     }
 }
