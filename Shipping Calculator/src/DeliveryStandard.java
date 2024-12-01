e0ee9a9d8b175cac85f9b616064f44777d9d9f12
kaurinz <906530+kaurinz@users.noreply.github.com>
2024-12-01 17:42:15 -0500

    convert DeliveryStandard to enum

--- Shipping Calculator/src/DeliveryStandard.java
+++ Shipping Calculator/src/DeliveryStandard.java
@@ -1,75 +1,32 @@
-//DeliveryStandard class definition file
+public enum DeliveryStandard {
+    STANDARD(1,"Standard",100, 200, 150, 1000,0),
+    EXPRESS(2,"Express",50, 150, 100, 500,50);
 
-public class DeliveryStandard {
-    private String type;
+    private final int identifier;
+    private final String type;
     private final double maxHeight;
     private final double maxLength;
     private final double maxWidth;
     private final double maxWeight;
-    private double surcharge;
+    private final double surcharge;
 
-    public static final DeliveryStandard STANDARD = CreateStandard("Standard");
-    public static final DeliveryStandard EXPRESS = CreateStandard("Express");
-
-    private static DeliveryStandard CreateStandard(String type){
-            DeliveryStandard newStandard = null;
-            switch (type.toLowerCase()){
-                case "standard":
-                    newStandard = new DeliveryStandard(100, 200, 150, 1000);
-                    newStandard.surcharge = 0;
-                    break;
-                case "express":
-                    newStandard = new DeliveryStandard(50, 150, 100, 500);
-                    newStandard.surcharge = 50;
-                    break;
-            }
-
-            if (newStandard != null){
-                newStandard.type = type;
-            }
-
-            return newStandard;
-    }
-
-    DeliveryStandard(double maxHeight, double maxLength, double maxWidth, double maxWeight) {
+    DeliveryStandard(int identifier, String type, double maxHeight, double maxLength, double maxWidth, double maxWeight, double surcharge){
+        this.identifier = identifier;
+        this.type = type;
         this.maxHeight = maxHeight;
         this.maxLength = maxLength;
         this.maxWidth = maxWidth;
         this.maxWeight = maxWeight;
+        this.surcharge = surcharge;
     }
 
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getType(){
-        return type;
-    }
-
-    public double getMaxHeight() {
-        return maxHeight;
-    }
-
-    public double getMaxLength() {
-        return maxLength;
-    }
-
-    public double getMaxWidth() {
-        return maxWidth;
-    }
-
-    public double getMaxWeight() {
-        return maxWeight;
-    }
-
-    public double getSurcharge() {
-        return surcharge;
-    }
-
-    @Override
-    public String toString(){
-        return this.type;
-    }
+    public String getType(){ return type; }
+    public int getIdentifier(){ return identifier; }
+    public double getMaxHeight() { return maxHeight; }
+    public double getMaxLength() { return maxLength; }
+    public double getMaxWidth() { return maxWidth; }
+    public double getMaxWeight() { return maxWeight; }
+    public double getSurcharge() { return surcharge; }
 
     public boolean meetsDimensions(double length, double width, double height){
         return (length <= this.maxLength) && (width <= this.maxWidth) && (height <= this.maxHeight);
@@ -77,5 +34,10 @@ public class DeliveryStandard {
 
     public boolean meetsWeight(double weight){
         return (weight <= this.maxWeight);
+    }
+
+    @Override
+    public String toString(){
+        return this.type;
     }
 }
