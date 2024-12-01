abe9c8b0e349b8336de87b3422977af76f8db566
kaurinz <906530+kaurinz@users.noreply.github.com>
2024-12-01 17:47:32 -0500

    reorganize City enum definition and add identifier property

--- Shipping Calculator/src/City.java
+++ Shipping Calculator/src/City.java
@@ -1,28 +1,30 @@
 public enum City {
-    MONTREAL(1,"Montreal","MTL"),
-    TORONTO(2, "Toronto","TOR"),
-    VANCOUVER(4, "Vancouver","VAN");
+    MONTREAL(1,1,"Montreal","MTL"),
+    TORONTO(2,2, "Toronto","TOR"),
+    VANCOUVER(3,4, "Vancouver","VAN");
 
-    private final int cityCode;
-    private final String cityName;
-    private final String cityTrackingCode;
+    private final int identifier;
+    private final int code;
+    private final String name;
+    private final String trackingCode;
 
-    City(int cityCode, String cityName, String trackingCode) {
-        this.cityCode = cityCode;
-        this.cityName = cityName;
-        this.cityTrackingCode = trackingCode;
+    City(int identifier, int cityCode, String cityName, String trackingCode) {
+        this.identifier = identifier;
+        this.code = cityCode;
+        this.name = cityName;
+        this.trackingCode = trackingCode;
     }
+
+    public String getTrackingCode(){ return this.trackingCode; }
+    public int getIdentifier() { return identifier; }
+    public String getName() { return name; }
 
     public int getPairCode(City cityR) {
-        return (this.cityCode | cityR.cityCode);
-    }
-
-    public String getTrackingCode(){
-        return this.cityTrackingCode;
+        return (this.code | cityR.code);
     }
 
     @Override
     public String toString(){
-        return this.cityName;
+        return this.name;
     }
 }
