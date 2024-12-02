710b8d7cf721e94e3ba69b9e319b90b326d9a7fd
kaurinz <906530+kaurinz@users.noreply.github.com>
2024-12-02 18:44:14 -0500

    remove directories and files no longer required

--- Shipping Calculator/src/dbutil/DBStorable.java
+++
@@ -1,9 +0,0 @@
-package dbutil;
-
-public interface DBStorable {
-    //Basic CRUD operations
-    public void createDBObject();
-    public void readDBObject();
-    public void updateDBObject();
-    public void deleteDBObject();
-}
