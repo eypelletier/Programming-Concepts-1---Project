3aef87e1eabf75fd4509aaf54ed2ad06cceb9693
kaurinz <906530+kaurinz@users.noreply.github.com>
2024-12-02 22:29:55 -0500

    removed commented out code from GoodsCategory

--- Shipping Calculator/src/GoodsCategory.java
+++ Shipping Calculator/src/GoodsCategory.java
@@ -9,37 +9,3 @@ public record GoodsCategory(int identifier, String name, double surchargeRate) {
         return this.name;
     }
 }
-
-/*public enum GoodsCategory {
-    REGULAR(1,"Regular",0),
-    FRAGILE(2,"Fragile",10),
-    HAZARDOUS(3,"Hazardous", 100),
-    EXPLOSIVE(4,"Explosive", 1000);
-
-    private final String name;
-    private final double surchargeRate;
-    private final int identifier;
-
-    GoodsCategory(int identifier,String name, double surchargeRate) {
-        this.name = name;
-        this.surchargeRate = surchargeRate;
-        this.identifier = identifier;
-    }
-
-    public double getSurchargeRate() {
-        return surchargeRate;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getIdentifier() {
-        return identifier;
-    }
-
-    @Override
-    public String toString() {
-        return this.name;
-    }
-}*/
