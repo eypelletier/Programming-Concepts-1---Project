2790ebc591ac941ea05434a31f12c0ec07d3b0c9
Eric Pelletier <2495211@edu.vaniercollege.qc.ca>
2024-12-04 10:45:49 -0500

    Added validation loop for startMenu in ShippingCalculator.java

--- Shipping Calculator/src/ShippingCalculator.java
+++ Shipping Calculator/src/ShippingCalculator.java
@@ -6,15 +6,27 @@ public class ShippingCalculator {
         System.out.println("Welcome to SuperShipper - Steam Early Release - pre-v0.1\n" +
                 "We will assist with calculating the cost of your shipment.");
 
-        OptionMenu startMenu = new OptionMenu();
-        String[][] menuOptions = {{"1","Create New Shipment"},{"2","Quit"}};
-        startMenu.addAllMenuOptions(menuOptions);
-        System.out.printf("\n%s\n", startMenu.withTitle("Main Menu").withDefault("1").menuAsString());
-        String menuChoice = startMenu.promptForChoice();
-        startMenu.isValidOption(menuChoice);
+        String startMenuChoice;
+        OptionMenu startMenu;
+        boolean validChoice = false;
+        do {
+            startMenu = new OptionMenu();
+            String[][] startMenuOptions = {{"1", "Create New Shipment"}, {"2", "Quit"}};
+            startMenu.addAllMenuOptions(startMenuOptions);
+            System.out.printf("\n%s\n", startMenu.withTitle("Main Menu").withDefault("1").menuAsString());
+            startMenuChoice = startMenu.promptForChoice();
+            startMenu.isValidOption(startMenuChoice);
+            if (startMenuChoice.equals("1") || startMenuChoice.equals("2")) {
+                validChoice = true;
+            }
+            else {
+                System.out.println("Invalid option. Try again.");
+            }
+        }
+        while (!validChoice);
 
         Shipment shipment = null;
-        switch (menuChoice) {
+        switch (startMenuChoice) {
             case "1":
                 shipment = ShipmentBuilder.buildShipment();
                 if (shipment != null) {
