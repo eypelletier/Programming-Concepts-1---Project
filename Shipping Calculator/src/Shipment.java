099ead140471161551f4f86da024eb2987b2e11e
kaurinz <906530+kaurinz@users.noreply.github.com>
2024-12-07 12:32:19 -0500

    add JavaDocs to Shipment

--- Shipping Calculator/src/Shipment.java
+++ Shipping Calculator/src/Shipment.java
@@ -1,6 +1,8 @@
 import java.util.*;
-import java.text.SimpleDateFormat;
 
+/** Shipment class
+ *
+ */
 public class Shipment {
     private String name, shipmentTrackingNumber;
     private City origin;
@@ -9,6 +11,9 @@ public class Shipment {
     private DeliveryStandard shippingSpeed;
     private final ArrayList<Package> shipmentPackages;
 
+    /** enum of problems that a specific package may encounter as part of the shipment
+     *
+     */
     public enum PackageProblems {
         MISMATCH_MODALITY,
         MISMATCH_STANDARD,
@@ -19,6 +24,9 @@ public class Shipment {
         GOODS_RESTRICTED_FOR_MODALITY,
     }
 
+    /** constructor
+     *
+     */
     public Shipment() {
         this.name = null;
         this.origin = null;
@@ -29,34 +37,68 @@ public class Shipment {
         this.shipmentTrackingNumber = null;
     }
 
+    /** gets the name of the shipment
+     *
+     * @return a string representing the name of the shipment
+     */
     public String getName() {
         return name;
     }
 
+    /** gets the origin of the shipment
+     *
+     * @return a City representing the origin for the shipment
+     */
     public City getOrigin() {
         return origin;
     }
 
+    /** gets the destination of the shipment
+     *
+     * @return a City representing the destination for the shipment
+     */
     public City getDestination() {
         return destination;
     }
 
+    /** get the unique pair code for destination & origin cities
+     *
+     * @return an integer representing the unique pair code for the destination and origin cities of the shipment
+     */
     public int getCityPairCode(){
         return origin.getPairCode(destination);
     }
 
+    /** gets the delivery modality (or method) for the shipment
+     *
+     * @return a DeliveryModality representing the shipment method
+     */
     public DeliveryModality getShippingMethod() {
         return shippingMethod;
     }
 
+    /** gets the delivery standard for the shipment
+     *
+     * @return a DeliveryStandard representing the delivery standard for the shipment
+     */
     public DeliveryStandard getShippingSpeed() {
         return shippingSpeed;
     }
 
+    /** gets the list of packages that form the shipment
+     *
+     * @return an ArrayList<Package> containing all packages of the shipment
+     */
     public ArrayList<Package> getPackages() {
         return shipmentPackages;
     }
 
+    /** gets a specific package according to its index in the package list
+     *  throws an error if provided package index is out of bounds
+     *
+     * @param packageNum, an integer representing the position of the package in the list
+     * @return a Package, representing the given package if found in the less
+     */
     public Package getPackageByIndex(int packageNum){
         int packageCount = shipmentPackages.size();
         if ( packageNum < 0 || packageNum >= packageCount) throw new IndexOutOfBoundsException("Package number out of bounds: " + packageNum);
@@ -64,65 +106,115 @@ public class Shipment {
         return shipmentPackages.get(packageNum);
     }
 
+    /** gets the tracking number for the shipment
+     *
+     * @return a String representing the tracking number for the shipment
+     */
     public String getShipmentTrackingNumber() {
         return shipmentTrackingNumber;
     }
 
+    /** sets the name for the shipment
+     *
+     * @param Name, a String representing the name for the shipment
+     */
     public void setName(String Name) {
         this.name = Name;
     }
 
+    /** sets the origin city for the shipment
+     *
+     * @param origin, a City representing the origin city of the shipment
+     */
     public void setOrigin(City origin) {
         this.origin = origin;
     }
 
+    /** sets the destination city for the shipment
+     *
+     * @param destination, a City representing the destination city for the shipment
+     */
     public void setDestination(City destination) {
         this.destination = destination;
     }
 
+    /** sets the shipping method
+     *
+     * @param shippingMethod, a DeliveryModality representing the shipment method to use
+     */
     public void setShippingMethod(DeliveryModality shippingMethod) {
         this.shippingMethod = shippingMethod;
     }
 
+    /** set the shipping speed (or standard) for the shipment
+     *
+     * @param shippingSpeed, a DeliveryStandard representing the standard to use for the shipment
+     */
     public void setShippingSpeed(DeliveryStandard shippingSpeed) {
         this.shippingSpeed = shippingSpeed;
     }
 
+    /** set the shipping tracking number
+     *
+     * @param trackingNumber, a String representing the tracking number for the shipment
+     */
     public void setShipmentTrackingNumber(String trackingNumber) {
         this.shipmentTrackingNumber = trackingNumber;
     }
 
-    public void addToPackages(Package pack) {
-        shipmentPackages.add(pack);
+    /** adds a package to the shipment
+     *
+     * @param pkg, a Package representing the package to add to the shipment
+     */
+    public void addToPackages(Package pkg) {
+        shipmentPackages.add(pkg);
     }
 
+
+    /** display (print to screen) the packages for the shipment
+     *
+     */
     public void displayPackages() {
         for (Package pack : shipmentPackages) {
             System.out.println(pack);
         }
     }
 
-    // Method to calculate shipping cost
-    public double calculateShippingCost() {
+    /** calculates the total shipping cost
+     *
+      * @return a double representing the total shipping cost
+     */
+    public double calculateTotalShippingCost() {
         double baseRate = calculateBaseRate();
         double surcharge = calculateSurcharge();
         return baseRate + surcharge;
     }
 
-    // Method to calculate the base rate of the shipment
+    /** calculate the base rate depending on the city pair for destination and origin
+     *
+     * @return a double representing the base rate for the shipment
+     */
     private double calculateBaseRate() {
         int montrealTorontoPairCode = City.MONTREAL.getPairCode(City.TORONTO);
         int montrealVancouverPairCode = City.MONTREAL.getPairCode(City.VANCOUVER);
+        int torontoVancouverPairCode = City.TORONTO.getPairCode(City.VANCOUVER);
 
         if (getCityPairCode() == montrealTorontoPairCode) {
             return 10.0;
+        } else if (getCityPairCode() == torontoVancouverPairCode) {
+            return 50.0;
         } else if (getCityPairCode() == montrealVancouverPairCode) {
             return 60.0;
         }
-        return 45.0;
+
+        //The shipment is within the city
+        return 5.0;
     }
 
-    // Method to calculate surcharge
+    /** calculate the total surcharge for the shipment
+     *
+     * @return a double representing the total surcharge for the shipment
+     */
     private double calculateSurcharge() {
         double surcharge = 0;
         for (Package pkg : shipmentPackages) {
@@ -138,7 +230,12 @@ public class Shipment {
         return surcharge;
     }
 
-    //Method to return any problematic packages according to constraints of DeliveryStandard and/or DeliveryModality
+    /** gets packages rejected during condition/validation checks according to constraints
+     * of DeliveryStandard and/or DeliveryModality
+     *
+     * @return a Map<Package, EnumSet<PackageProblems>> representing a mapping of packages to problems related
+     * to the package
+     */
     public Map<Package, EnumSet<PackageProblems>> getRejectedPackages(){
         Map<Package, EnumSet<PackageProblems>> packagesRejected = new HashMap<>();
         int packageCount = shipmentPackages.size();
@@ -181,7 +278,10 @@ public class Shipment {
         return packagesRejected;
     }
 
-    //Method to get the total weight of the shipment
+    /** gets the total weight for the shipment
+     *
+     * @return a double representing the total weight for the shipment
+     */
     public double getTotalWeight(){
         double totalWeight = 0.0;
         for (Package pkg : shipmentPackages) {
@@ -190,7 +290,10 @@ public class Shipment {
         return totalWeight;
     }
 
-    //Get Weight Surcharge
+    /** gets the total surcharge related to exceeding maximum weight of standard or method of shipment
+     *
+     * @return a double representing the total weight surcharge in effect
+     */
     public double getWeightSurcharge(){
         double totalWeight = getTotalWeight();
         double totalSurchage = 0.0;
@@ -198,17 +301,27 @@ public class Shipment {
         if ( totalWeight > shippingSpeed.getMaxWeight() ) totalSurchage+=50;
         return totalSurchage;
     }
-    //Get Surcharge for Priority
+
+    /** gets surcharge related to shipping speed/standard
+     *
+     * @return a double representing the surcharge related to shipping speed/standard
+     */
     public double getPrioritySurcharge(){
         return shippingSpeed.getSurcharge();
     }
 
-    //Get Surcharge for Method
+    /** gets surcharge related to shipping method
+     *
+     * @return a double representing the surcharge related to shipping method
+     */
     public double getModalitySurcharge(){
         return shippingMethod.getSurcharge();
     }
 
-    //Get BaseRate
+    /** gets the base rate for the shipment
+     *
+     * @return a double representing the base rate for the shipment
+     */
     public double getBaseRate() {
         return calculateBaseRate();
     }
